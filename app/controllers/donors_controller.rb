75fc1398be61c9caf5331a56f9a3b7ffcddf2220
sarjumulmi <mulmi.sarju@gmail.com>
2017-07-11 15:55:33 -0400

    sign up controller action added

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -1,11 +1,24 @@
 class DonorsController < ApplicationController
 
-    get '/signup' do
-      if !logged_in?
-        erb :'/donors/signup'
-      else
-        redirect '/donations'
-      end
+  get '/signup' do
+    if !logged_in?
+      erb :'/donors/signup'
+    else
+      redirect '/donations'
     end
+  end
+
+  post '/donors' do
+    # binding.pry
+    donor = Donor.new(params)
+    if donor.save
+      session[:user_id] = donor.id
+      redirect '/donations'
+    else
+      flash[:message] = 'Please fill all the required fields.'
+      erb :'/donors/signup'
+    end
+  end
+
 
 end
