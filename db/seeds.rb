8f30fb863f7214ac53c9a6128fec428677a22a0b
sarjumulmi <mulmi.sarju@gmail.com>
2017-07-12 23:11:54 -0400

    seed file fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@
   Charity.create(:name=>"charity#{n}")
 end
 
-donation1 = Donation.create(:item=>"item1", :item_price=>100, :donor=>Donor.find(1), :charity=>Charity.find(1))
-donation2 = Donation.create(:item=>"item2", :item_price=>1000, :donor=>Donor.find(1), :charity=>Charity.find(2))
-donation3 = Donation.create(:item=>"item3", :item_price=>1500, :donor=>Donor.find(2), :charity=>Charity.find(3))
-donation4 = Donation.create(:item=>"item4", :item_price=>2100, :donor=>Donor.find(3), :charity=>Charity.find(3))
-donation5 = Donation.create(:item=>"item5", :item_price=>1200, :donor=>Donor.find(2), :charity=>Charity.find(1))
-donation6 = Donation.create(:item=>"item6", :item_price=>300, :donor=>Donor.find(3), :charity=>Charity.find(2))
+donation1 = Donation.create(:item=>"item1", :item_price=>100, :donor=>Donor.find_by(:name=>"donor1"), :charity=>Charity.find_by(:name=>"charity1"))
+donation2 = Donation.create(:item=>"item2", :item_price=>1000, :donor=>Donor.find_by(:name=>"donor1"), :charity=>Charity.find_by(:name=>"charity2"))
+donation3 = Donation.create(:item=>"item3", :item_price=>1500, :donor=>Donor.find_by(:name=>"donor2"), :charity=>Charity.find_by(:name=>"charity0"))
+donation4 = Donation.create(:item=>"item4", :item_price=>2100, :donor=>Donor.find_by(:name=>"donor0"), :charity=>Charity.find_by(:name=>"charity0"))
+donation5 = Donation.create(:item=>"item5", :item_price=>1200, :donor=>Donor.find_by(:name=>"donor2"), :charity=>Charity.find_by(:name=>"charity1"))
+donation6 = Donation.create(:item=>"item6", :item_price=>300, :donor=>Donor.find_by(:name=>"donor0"), :charity=>Charity.find_by(:name=>"charity2"))
