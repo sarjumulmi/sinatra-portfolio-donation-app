4a9a02c38b912403fc0ed7ce7d0f17b7d0260e48
sarjumulmi <mulmi.sarju@gmail.com>
2017-07-14 12:11:02 -0400

    Update donations_controller.rb

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -11,10 +11,10 @@ class DonationsController < ApplicationController
   post '/donations' do
     # binding.pry
     # params = {"charity_id"=>"1", "item"=>"somthing", "item_price"=>"4000"}
-    donation = Donation.create(:item=>params[:item], :item_price=>params[:item_price].to_i)
+    donation = Donation.create(:item=>params[:item], :item_price=>params[:item_price].to_i)#=> check if you can save donation, if not use flahs message
     donation.donor = current_user
     donation.charity = Charity.find(params[:charity_id])
-    donation.save
+    donation.save 
     redirect "/donors/#{current_user.id}"
   end
 
