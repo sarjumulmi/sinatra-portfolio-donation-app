be35113ac2079214d3bae580fef6d896df278e8a
sarjumulmi <mulmi.sarju@gmail.com>
2017-07-15 11:22:28 -0400

    warning message added do new donation form

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -11,11 +11,19 @@ class DonationsController < ApplicationController
   post '/donations' do
     # binding.pry
     # params = {"charity_id"=>"1", "item"=>"somthing", "item_price"=>"4000"}
-    donation = Donation.create(:item=>params[:item], :item_price=>params[:item_price].to_i)#=> check if you can save donation, if not use flahs message
-    donation.donor = current_user
-    donation.charity = Charity.find(params[:charity_id])
-    donation.save 
-    redirect "/donors/#{current_user.id}"
+    #=> check if you can save donation, if not use flahs message
+    if donation = Donation.create(:item=>params[:item], :item_price=>params[:item_price].to_i)
+      donation.donor = current_user
+      donation.charity = Charity.find(params[:charity_id])
+      donation.save
+      redirect "/donors/#{current_user.id}"
+    else
+      flash[:message] = "Please fill all the fields."
+      erb :'/donations/new'
+    end
+
+
+
   end
 
 end
