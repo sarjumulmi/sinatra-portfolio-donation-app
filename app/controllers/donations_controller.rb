54575702b5e49051f17c00464923062328ae4c78
sarjumulmi <mulmi.sarju@gmail.com>
2017-07-17 18:53:26 -0400

    fixed new donation flash message

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -12,7 +12,9 @@ class DonationsController < ApplicationController
     # binding.pry
     # params = {"charity_id"=>"1", "item"=>"somthing", "item_price"=>"4000"}
     #=> check if you can save donation, if not use flahs message
-    if donation = Donation.create(:item=>params[:item], :item_price=>params[:item_price].to_i)
+    donation = Donation.new(:item=>params[:item], :item_price=>params[:item_price].to_i)
+    if donation.save
+      # binding.pry
       donation.donor = current_user
       donation.charity = Charity.find(params[:charity_id])
       donation.save
